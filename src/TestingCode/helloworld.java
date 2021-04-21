6601f26866f5797f8601d1250100bd97a266f128
duberg <duberg.hanna@gmail.com>
2021-04-21 15:27:17 +0200

    den tredje

--- src/TestingCode/helloworld.java
+++ src/TestingCode/helloworld.java
@@ -3,6 +3,6 @@ package TestingCode;
 public class helloworld {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Hej World 2");
+        System.out.println("Hej World 3");
     }
 }
